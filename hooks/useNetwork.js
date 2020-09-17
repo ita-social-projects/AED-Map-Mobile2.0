4593b821b70427be8718da91397e00d08219a89a
39312657 <tacjka34@gmail.com>
2020-09-17 13:36:02 +0300

    Refactoring of redux, hooks and utils

--- hooks/useNetwork.js
+++ hooks/useNetwork.js
@@ -1,10 +1,12 @@
 import NetInfo from '@react-native-community/netinfo';
 import {ErrorAlertGenerator} from "../utils/alerts";
+import {networkErrorTitle,networkErrorText} from '../config'
+
 
 const useNetwork = () => {
     NetInfo.addEventListener((state) => {
         if (!state.isConnected) {
-            ErrorAlertGenerator("Мережа","Не вдалось під'єднатись до мережі");
+            ErrorAlertGenerator(networkErrorTitle,networkErrorText);
         }
     })
 };
