85ad84e5e137167ddba8a7ed17ad7d92b54e2e65
Dmytro Vasyliev <vas.mytro@gmail.com>
2020-10-12 17:53:24 +0300

    fixed search panel styles for android

--- components/search/search.js
+++ components/search/search.js
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import {useDispatch,useSelector} from 'react-redux';
+import { useDispatch,useSelector } from 'react-redux';
 import { setSearchLocation } from '../../redux/actions';
 import * as Location from 'expo-location';
 import {
@@ -10,11 +10,12 @@ import {
 import { SearchBar } from 'react-native-elements';
 import nearestDeff from '../../utils/nearestDeff';
 import { getDeff, setSelectedDeff } from '../../redux/actions';
-import {appConfig, searchPanelConfig} from "../../config";
+import { appConfig, searchPanelConfig } from "../../config";
+
+const isAndroidOS = Platform.OS === 'android'
 
 const Search = () => {
   const dispatch = useDispatch()
-
   const [search, setSearch] = useState('')
 
   const { deffData} = useSelector((state) => ({
@@ -24,28 +25,34 @@ const Search = () => {
   const handleSearchChange = async (text) => {
     setSearch(text);
     const searchLocation = await Location.geocodeAsync(text);
-    const searchLocationArray = [searchLocation[0].longitude, searchLocation[0].latitude];
-    const nearbyDefs = nearestDeff(deffData,searchLocationArray);
-    if(nearbyDefs.length) {
-      dispatch(setSelectedDeff(nearbyDefs[0].id));
-      dispatch(getDeff(nearbyDefs[0].id))
+    if(searchLocation[0]) {
+      const searchLocationArray = [searchLocation[0].longitude, searchLocation[0].latitude];
+      const nearbyDefs = nearestDeff(deffData,searchLocationArray);
+      if(nearbyDefs.length) {
+        dispatch(setSelectedDeff(nearbyDefs[0].id));
+        dispatch(getDeff(nearbyDefs[0].id))
+      }
+      dispatch(
+        setSearchLocation(searchLocationArray)
+      )
     }
-    dispatch(
-      setSearchLocation(searchLocationArray)
-    )
   };
 
     return (
         <View style={styles.searchBar}>
           <SearchBar
             round
-            searchIcon={{ size: 24 }}
+            searchIcon={styles.icon}
+            cancelIcon={styles.icon}
+            clearIcon={styles.icon}
             onChangeText={text => handleSearchChange(text)}
             placeholder="Type Here..."
             value={search}
             platform={Platform.OS}
             cancelButtonTitle='Cancel'
-            containerStyle={{backgroundColor: '#282c34'}}
+            containerStyle={styles.container}
+            inputStyle={ isAndroidOS && styles.input }
+            placeholderTextColor={ isAndroidOS && searchPanelConfig.placeholderColor }
           />
         </View>
     )
@@ -58,6 +65,19 @@ const styles = StyleSheet.create({
     backgroundColor: appConfig.backgroundColor,
     paddingTop: searchPanelConfig.paddingTop
   },
+  container: {
+    backgroundColor: '#282c34'
+  },
+  input: {
+    backgroundColor: '#e7eae7',
+    borderRadius: 8,
+    height: 50,
+    paddingHorizontal: 10
+  },
+  icon: {
+    color: isAndroidOS && searchPanelConfig.inputColor,
+    fontSize: 24
+  }
 });
 
 export default Search
