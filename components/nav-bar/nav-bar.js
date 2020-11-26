f3193298d7a20c56f04f15fb38d1141c6dcb62a5
Nadiya Fomenko <47325620+nadiyafomenko@users.noreply.github.com>
2020-11-26 14:31:24 +0200

    added active button background

--- components/nav-bar/nav-bar.js
+++ components/nav-bar/nav-bar.js
@@ -12,6 +12,7 @@ import {appConfig, navBarConfig} from "../../config";
 
 const MoveTypes = () => {
   const dispatch = useDispatch();
+  const [active, setActive] = useState(0);
 
   const currentDeff = useSelector(state => state.currentDeff);
 
@@ -25,27 +26,30 @@ const MoveTypes = () => {
     <Animated.View style={[styles.driveTypes]}>
       <TouchableOpacity onPress={
         () => {
+          setActive(0)
           dispatch(setDrivingMode('driving')),
           setAsDestination()
         }
       }>
-        <View style={styles.driveTypeButton}>
+        <View style={active === 0 ? styles.driveTypeButtonActive : styles.driveTypeButton }>
           <Image
-            style={styles.driveImg}
+            style={ styles.driveImg }
             source={require('../../assets/move-types/directions_car.png')}
           />
         </View>
       </TouchableOpacity>
 
-      <TouchableOpacity onPress={
+      <TouchableOpacity
+          onPress={
         () => {
+          setActive(1)
           dispatch(setDrivingMode('cycling')),
           setAsDestination()
         }
       }>
-        <View style={styles.driveTypeButton}>
+        <View style={active === 1 ? styles.driveTypeButtonActive : styles.driveTypeButton }>
           <Image
-            style={styles.driveImg}
+            style={ styles.driveImg }
             source={require('../../assets/move-types/directions_bicycle.png')}
           />
         </View>
@@ -53,17 +57,19 @@ const MoveTypes = () => {
 
       <TouchableOpacity onPress={
         () => {
+          setActive(2)
           dispatch(setDrivingMode('walking')),
           setAsDestination()
         }
       }>
-        <View style={styles.driveTypeButton}>
+        <View style={active === 2 ? styles.driveTypeButtonActive : styles.driveTypeButton }>
           <Image
-            style={styles.driveImg}
+              style={ styles.driveImg }
             source={require('../../assets/move-types/directions_pedestrian.png')}
           />
         </View>
       </TouchableOpacity>
+
       <TouchableOpacity onPress={
         () => {
           dispatch(setDrivingMode(null));
@@ -94,11 +100,20 @@ const styles = StyleSheet.create({
     width: '100%'
   },
   driveTypeButton: navBarConfig.typeButtonStyle(3),
+  driveTypeButtonActive: {
+    width: 40,
+    height: 40,
+    padding: 2,
+    textAlign: "center",
+    margin: 'auto',
+    backgroundColor: appConfig.backgroundActiveColor,
+    borderRadius: 25,
+  },
   closeTypeButton: navBarConfig.typeButtonStyle(5),
   driveImg: {
-    width: '100%',
-    height: '100%',
-    tintColor: '#fff'
+    width: 30,
+    height: 30,
+    tintColor: '#fff',
   },
   closeImg: {
     width: '100%',
